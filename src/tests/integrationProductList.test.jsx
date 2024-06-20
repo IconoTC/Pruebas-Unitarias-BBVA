9ab6cec0ee67942a9a581485115309866e7a9a3b
Javier Flores <javifb1983@gmail.com>
2024-06-20 12:34:29 +0200

    for each - rendeer product list

--- src/tests/integrationProductList.test.jsx
+++ src/tests/integrationProductList.test.jsx
@@ -40,4 +40,30 @@ describe('ProductList', () => {
               });
           });
       });
+
+      // Test para verificar que se muestra un producto individual al hacer clic en un botón
+    test('should fetch and display a single product on button click', async () => {
+      render(<ProductList />);
+      // Verifica que el producto no esté presente antes de hacer clic en el botón
+      expect(screen.queryByLabelText('Mens Casual Premium Slim Fit T-Shirts')).not.toBeInTheDocument();
+      fireEvent.click(screen.getByText('Click me')); // Simula el clic en el botón
+      // Espera a que se muestre el producto
+      await waitFor(() => {
+          // Verifica que console.log se haya llamado con los datos del producto esperado
+          expect(console.log).toHaveBeenCalledWith({
+              "id": 2,
+              "title": "Mens Casual Premium Slim Fit T-Shirts ",
+              "price": 22.3,
+              "description": "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
+              "category": "men's clothing",
+              "image": "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
+              "rating": {
+                  "rate": 4.1,
+                  "count": 259
+              }
+          });
+          // Verifica que el elemento del producto esté presente en la pantalla
+          expect(screen.getByLabelText('Mens Casual Premium Slim Fit T-Shirts')).toBeInTheDocument();
+      });
+  });
   });
