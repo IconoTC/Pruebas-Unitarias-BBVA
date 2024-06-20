ecc5dbb538fe5c006af314b7ddce61f37a3005d7
Javier Flores <javifb1983@gmail.com>
2024-06-20 13:36:26 +0200

    remember checkbox works properly

--- src/tests/home.test.jsx
+++ src/tests/home.test.jsx
@@ -1,22 +1,22 @@
-import { afterAll, beforeAll, describe, expect, test, vi } from "vitest";
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Home } from '../pages/Home';
 
-describe('Home component', () => {
-    beforeAll(() => {
+describe('Test Home component', () => {
+
+    beforeEach(() => {
         vi.spyOn(console, 'log').mockImplementation(() => {});
         vi.spyOn(window, 'alert').mockImplementation(() => {});
     });
 
-    afterAll(() => {
+    afterEach(() => {
         console.log.mockRestore();
         window.alert.mockRestore();
     });
 
     test('renders sign-in form', () => {
         render(<Home />);
-        
         // Verificar que los elementos del formulario estén en el documento
         expect(screen.getByLabelText(/email address/i)).toBeInTheDocument();
         expect(screen.getByLabelText(/password input/i)).toBeInTheDocument();
@@ -25,4 +25,20 @@ describe('Home component', () => {
         expect(screen.getByText(/forgot password\?/i)).toBeInTheDocument();
         expect(screen.getByText(/don't have an account\? sign up/i)).toBeInTheDocument();
     });
+
+    test('Test remember me checkbox', () => {
+      render(<Home />);
+      // Obtener el checkbox
+      const rememberMeCheckbox = screen.getByLabelText(/remember me/i);
+      // Verificar que el checkbox está inicialmente desmarcado
+      expect(rememberMeCheckbox.checked).toBe(false);
+      // Simular el marcado del checkbox
+      fireEvent.click(rememberMeCheckbox);
+      // Verificar que el checkbox está marcado
+      expect(rememberMeCheckbox.checked).toBe(true);
+      // Simular el desmarcado del checkbox
+      fireEvent.click(rememberMeCheckbox);
+      // Verificar que el checkbox está desmarcado
+      expect(rememberMeCheckbox.checked).toBe(false);
+  });
   });
