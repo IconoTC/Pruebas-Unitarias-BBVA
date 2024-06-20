5add370fef14370e3b6cd7357351eddb1d58d6b3
Javier Flores <javifb1983@gmail.com>
2024-06-20 10:28:41 +0200

    render switch and change states correctly

--- src/tests/muiSwitch.test.jsx
+++ src/tests/muiSwitch.test.jsx
@@ -10,4 +10,13 @@ describe("Test SwitchMui", () => {
     const switchElement = screen.getByRole('checkbox'); // Busca el elemento Switch por su rol de checkbox
     expect(switchElement).toBeInTheDocument(); // Verifica que el Switch está en el documento
   });
+
+    //verificará si se llama a la propiedad onChange cuando cambia el estado de algún componente
+    test('calls onChange prop when state changes', () => { 
+      const handleChange = vi.fn(); // Crea una función mock para el evento onChange
+      render(<Switch onChange={handleChange} />); // Renderiza el Switch con el evento onChange
+      const switchElement = screen.getByRole('checkbox'); // Busca el elemento Switch por su rol de checkbox
+      fireEvent.click(switchElement); // Simula un clic en el Switch
+      expect(handleChange).toHaveBeenCalledTimes(1); // Verifica que la función handleChange se llamó una vez
+    });
 });
